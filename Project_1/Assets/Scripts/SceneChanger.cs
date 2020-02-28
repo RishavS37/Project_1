a2383f97d474a034f650f7b54156a35000e7fba4
RishavS <rishavsardhalia37@gmail.com>
2020-02-28 19:10:44 +0530

    Created 3 new scenes

---
+++ Project_1/Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    // Start is called before the first frame update
+ 
+
+    public void Load_Scene1()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Load_Scene2()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void Load_Scene3()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void Load_MasterScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
